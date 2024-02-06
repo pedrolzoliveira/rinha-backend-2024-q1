2759df3f15001f558fbaadbc6a8d8f1ca4fa3a3f
Pedro Oliveira <pluiz@mychirotouch.com>
2024-02-05 22:54:16 -0300

    use db const in the same file

--- src/database-client.mjs
+++
@@ -1,5 +0,0 @@
-import pg from 'pg';
-
-export const db = new pg.Client('postgres://postgres:1234@localhost/rinha');
-
-await db.connect();
--- src/server.mjs
+++ src/server.mjs
@@ -1,5 +1,9 @@
 import http from 'http';
-import { db } from './database-client.mjs';
+import pg from 'pg';
+
+const db = new pg.Client('postgres://postgres:1234@localhost/rinha');
+
+await db.connect();
 
 /**
  * 
