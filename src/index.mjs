69c79a9ce2556855aa22c477e647e6c825012862
Pedro Oliveira <pluiz@mychirotouch.com>
2024-02-05 21:45:24 -0300

    add agradecimento

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import os from 'os';
 import cluster from 'cluster';
 
+// Obrigado Erick Wendel
 const runPrimaryProcess = () => {
   const processCount = os.cpus().length * 2;
   for (let i = 0; i < processCount; i++) {
